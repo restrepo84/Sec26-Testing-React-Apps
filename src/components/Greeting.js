f84b637524153f5b2d2395c66695d8835d97f788
restrepo84 <112986175+restrepo84@users.noreply.github.com>
2023-03-30 17:16:41 -0600

    first testing

---
+++ src/components/Greeting.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// import styles from './Greeting.module.css';
+
+const Greeting = () => {
+  return (
+    <div>
+      <h2>Hello World!</h2>
+      <p>It's good to see you!</p>
+    </div>
+  );
+};
+
+export default Greeting;
